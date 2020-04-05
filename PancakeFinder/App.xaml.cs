4cc1040e5e3d0c04a3aba5dfb1e4ca4b7ac076d4
Luce Carter <luce@lucyscarter.co.uk>
2020-04-05 13:39:22 +0100

    Started app theme

--- PancakeFinder/App.xaml.cs
+++ PancakeFinder/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using PancakeFinder.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace PancakeFinder
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new PancakePage();
         }
 
         protected override void OnStart()
